cb3fda11cc41f5f6cd8304b557ac2c6963824ae9
sk <saravana78120@gmail.com>
2026-02-01 20:06:51 +0530

    Implement basic USART RX using polling.

--- Inc/peripherals.h
+++ Inc/peripherals.h
@@ -1,7 +1,11 @@
 #ifndef PERIPHERALS_H_
 #define PERIPHERALS_H_
 
+#include <string.h>
 #include "stm32f4xx.h"
+
+/* Macro to calculate length of an Array */
+#define ARR_LEN(arr) (sizeof(arr)/sizeof(arr[0]))
 
 extern volatile uint32_t msTicks;
 
@@ -9,6 +13,7 @@ void rcc_init(void);
 void gpio_init(void);
 void usart_init(void);
 void usart_transmit_polling(uint8_t *pData, uint32_t size, uint32_t timeout);
+void usart_receive_polling(uint8_t *pData, uint32_t size, uint32_t timeout);
 
 static inline void ms_delay(uint32_t delay) {
 	uint32_t init_val = msTicks;
--- Src/main.c
+++ Src/main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 volatile uint32_t msTicks;
-uint8_t tx_buff[10] = "SARAVANA!";
+uint8_t rx_buff[10];
 
 int main(void) {
 	rcc_init();
@@ -10,11 +10,16 @@ int main(void) {
 	usart_init();
 
 	while(1) {
+		usart_receive_polling(rx_buff, ARR_LEN(rx_buff), 100);
+
+		if(*rx_buff) {
+			usart_transmit_polling(rx_buff, ARR_LEN(rx_buff), 100);
+
+			memset(rx_buff, 0, ARR_LEN(rx_buff));
+		}
+
 		// Read PA0 for Low Signal
 		if((GPIOA->IDR & (1UL << 0)) == 0) {
-
-			usart_transmit_polling(tx_buff, 100, 10);
-
 			// Toggle PC13
 			GPIOC->ODR ^= (1UL << 13);
 			ms_delay(200);
